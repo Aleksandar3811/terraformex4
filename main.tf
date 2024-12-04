f6a3c1c260dbf3afb2d7c58d7b4926528d03b745
Aleksandar3811 <alek.kondrov@gmail.com>
2024-12-04 20:30:48 +0200

    fix

--- main.tf
+++ main.tf
@@ -2,46 +2,37 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "4.11.0"
+      version = "4.12.0"
     }
-  }
-  backend "azurerm" {
-    resource_group_name  = "StorageRG"
-    storage_account_name = "taskboardstoragedimomite"
-    container_name       = "taskboardstoragedimocontainer"
-    key                  = "terraform.tfstate"
   }
 }
 
 provider "azurerm" {
-  subscription_id = "6caddd11-60e3-4fad-93af-dd36715ea8c7"
+  subscription_id = "2458a123-e190-48f7-9097-e8664d2ab9ee"
   features {
+
   }
 }
-
 resource "random_integer" "ri" {
   min = 10000
   max = 99999
 }
-
-resource "azurerm_resource_group" "dimorg" {
+resource "azurerm_resource_group" "alekrg" {
+  name     = "${var.app_service_name}-${random_integer.ri.result}"
   location = var.resource_location
-  name     = "${var.resource_group_name}-${random_integer.ri.result}"
 }
-
-resource "azurerm_service_plan" "dimoasp" {
+resource "azurerm_service_plan" "alekasp" {
   name                = "${var.app_service_plan_name}-${random_integer.ri.result}"
-  resource_group_name = azurerm_resource_group.dimorg.name
-  location            = azurerm_resource_group.dimorg.location
+  resource_group_name = azurerm_resource_group.alekrg.name
+  location            = azurerm_resource_group.alekrg.location
   os_type             = "Linux"
   sku_name            = "F1"
 }
-
-resource "azurerm_linux_web_app" "dimoalwa" {
-  name                = "${var.app_service_name}${random_integer.ri.result}"
-  resource_group_name = azurerm_resource_group.dimorg.name
-  location            = azurerm_resource_group.dimorg.location
-  service_plan_id     = azurerm_service_plan.dimoasp.id
+resource "azurerm_linux_web_app" "alekwa" {
+  name                = "${var.app_service_name}-${random_integer.ri.result}"
+  resource_group_name = azurerm_resource_group.alekrg.name
+  location            = azurerm_service_plan.alekasp.location
+  service_plan_id     = azurerm_service_plan.alekasp.id
 
   site_config {
     application_stack {
@@ -52,38 +43,35 @@ resource "azurerm_linux_web_app" "dimoalwa" {
   connection_string {
     name  = "DefaultConnection"
     type  = "SQLAzure"
-    value = "Data Source=tcp:${azurerm_mssql_server.sqlserverdimo.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_mssql_database.dimodatabase.name};User ID=${azurerm_mssql_server.sqlserverdimo.administrator_login};Password=${azurerm_mssql_server.sqlserverdimo.administrator_login_password};Trusted_Connection=False; MultipleActiveResultSets=True;"
+    value = "Data Source=tcp:${azurerm_mssql_server.alekmssqls.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_mssql_database.alekmssqld.name};User ID=${azurerm_mssql_server.alekmssqls.administrator_login};Password=${azurerm_mssql_server.alekmssqls.administrator_login_password};Trusted_Connection=False; MultipleActiveResultSets=True;"
   }
 }
-
-resource "azurerm_mssql_server" "sqlserverdimo" {
-  name                         = var.sql_server_name
-  resource_group_name          = azurerm_resource_group.dimorg.name
-  location                     = azurerm_resource_group.dimorg.location
+resource "azurerm_mssql_server" "alekmssqls" {
+  name                         = "${var.sql_server_name}-${random_integer.ri.result}"
+  resource_group_name          = azurerm_resource_group.alekrg.name
+  location                     = azurerm_resource_group.alekrg.location
   version                      = "12.0"
   administrator_login          = var.sql_administrator_login_uusername
   administrator_login_password = var.sql_administrator_login_password
 }
 
-resource "azurerm_mssql_database" "dimodatabase" {
-  name           = var.sql_database_name
-  server_id      = azurerm_mssql_server.sqlserverdimo.id
+resource "azurerm_mssql_database" "alekmssqld" {
+  name           = "${var.sql_database_name}-${random_integer.ri.result}"
+  server_id      = azurerm_mssql_server.alekmssqls.id
   collation      = "SQL_Latin1_General_CP1_CI_AS"
   license_type   = "LicenseIncluded"
   max_size_gb    = 2
   zone_redundant = false
   sku_name       = "S0"
 }
-
-resource "azurerm_mssql_firewall_rule" "dimofirewall" {
+resource "azurerm_mssql_firewall_rule" "alekfw" {
   name             = var.firewall_rule_name
-  server_id        = azurerm_mssql_server.sqlserverdimo.id
+  server_id        = azurerm_mssql_server.alekmssqls.id
   start_ip_address = "0.0.0.0"
   end_ip_address   = "0.0.0.0"
 }
-
-resource "azurerm_app_service_source_control" "github" {
-  app_id                 = azurerm_linux_web_app.dimoalwa.id
+resource "azurerm_app_service_source_control" "aleksc" {
+  app_id                 = azurerm_linux_web_app.alekwa.id
   repo_url               = var.github_repo_url
   branch                 = "main"
   use_manual_integration = true
